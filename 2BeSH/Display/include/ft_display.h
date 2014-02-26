9def8381385e4807caea44a2f13a2a66de6deec4
Alexandre MERLE <amerle@e3r2p16.42.fr>
2014-02-26 08:36:28 +0100

    patch

--- 2BeSH/Display/include/ft_display.h
+++ 2BeSH/Display/include/ft_display.h
@@ -1,6 +1,13 @@
 #ifndef FT_DISPLAY_H
 # define FT_DISPLAY_H
 
+typedef struct s_display	t_display;
 
+struct	s_display
+{
+	t_display	*(*doEvent)(char *c);
+};
+
+t_display	*event_singleton();
 
 #endif
--- 2BeSH/Display/src/ft_display.c
+++ 2BeSH/Display/src/ft_display.c
@@ -1,1 +1,21 @@
 #include "ft_display.h"
+#include <stdlib.h>
+
+static void	display_init(t_display *dis)
+{
+	(void)dis;
+}
+
+t_display	*display_singleton()
+{
+	static t_display	*singleton = NULL;
+
+	if (!singleton)
+	{
+		singleton = (t_display*)malloc(sizeof(t_display));
+		if (!singleton)
+			exit(1);
+		display_init(singleton);
+	}
+	return (singleton);
+}
