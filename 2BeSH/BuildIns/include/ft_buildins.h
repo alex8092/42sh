5d0839abc372b987e6983d24a63ebdba63f71e58
Alexandre MERLE <amerle@e3r2p9.42.fr>
2014-02-27 06:21:42 +0100

    patch

--- 2BeSH/BuildIns/include/ft_buildins.h
+++ 2BeSH/BuildIns/include/ft_buildins.h
@@ -9,7 +9,6 @@ typedef struct s_buildin	t_buildin;
 struct		s_buildins
 {
 	t_buildin	*m_begin;
-	t_buildins	*(*add_buildin)(char *name, void (*)(char **args));
 	t_bool		(*is_buildin)(char *);
 };
 
