ef62abe9016d9924ffb62bbf40289c8ae7bcf83e
Thibault RIVIERE <thrivier@e3r3p9.42.fr>
2014-02-27 07:20:51 +0100

    Message par default

--- 2BeSH/Parser/include/ft_parser.h
+++ 2BeSH/Parser/include/ft_parser.h
@@ -1,12 +1,13 @@
 #ifndef FT_PARSER_H
 # define FT_PARSER_H
+# include "ft_operation.h"
 
 typedef struct s_parser	t_parser;
 
 struct		s_parser
 {
-	void	(*start)();
-	void	(*parse)(char *);
+	void			(*start)();
+	void			(*parse)(char *);
 };
 
 t_parser	*parser_singleton();
--- 2BeSH/Parser/src/ft_parser_parse.c
+++ 2BeSH/Parser/src/ft_parser_parse.c
@@ -5,40 +5,53 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void	p_parser_parse(char *str)
+static void		ft_put_dbl_op(char c, t_operation **cmd_op)
+{
+	if (c == '|')
+		(*cmd_op)->op = OP_DBL_PIPE;
+	else
+		(*cmd_op)->op = OP_DBL_EP;
+}
+
+static void		ft_put_single_op(char c, t_operation **cmd_op)
+{
+	if (c == ';')
+		(*cmd_op)->op = OP_PV;
+	else if (c == '|')
+		(*cmd_op)->op = OP_PIPE;
+	else
+		(*cmd_op)->op = OP_EP;
+}
+
+static void		ft_check_op(char c, char d, t_operation **op)
+{
+	if (c == d)
+		ft_put_dbl_op(c, op);
+	else
+		ft_put_single_op(c, op);
+}
+
+void	 		p_parser_parse(char *str)
 {
 	int			i;
 	char		*tmp;
 	t_operation	*cmd_op;
 	t_operation	*begin;
+	t_operation	*last;
 
 	tmp = str;
-	cmd_op = ft_init_operation();
 	begin = cmd_op;
 	while ((i = ft_findfirstof(tmp, ";|&")) != -1)
 	{
-		if (tmp[i] == ';' || tmp[i] == '|' || tmp[i + 1] == tmp[i])
-		{
-			cmd_op->str = ft_strndup(str, (i + (tmp - str)));
-			if (tmp[i] == tmp[i + 1])
-			{
-				if (tmp[i] == '|')
-					cmd_op->op = OP_DBL_PIPE;
-				else
-					cmd_op->op = OP_DBL_EP;
-				++i;
-			}
-			else
-			{
-				if (tmp[i] == ';')
-					cmd_op->op = OP_PV;
-				else if (tmp[i] == '|')
-					cmd_op->op = OP_PIPE;
-				else
-					cmd_op->op = OP_EP;
-			}
-			str = tmp + i + 1;
-		}
-		tmp = tmp + i + 1;
+		cmd_op = ft_init_operation();
+		cmd_op->str = ft_strndup(str, (i + (tmp - str)));
+		ft_check_op(tmp[i], tmp[i + 1], &cmd_op);
+		++i;
+		last->next = cmd_op;
+		last = cmd_op;
+		str = tmp + i + 1;
+		tmp = tmp +i + 1;
 	}
+	last->op = OP_END;
+	last->str = ft_strdup(tmp);
 }
