678217611e165360da78d496b2b8926c5ad62e1f
Theotime RIVIERE <triviere@e3r3p13.42.fr>
2014-02-27 03:02:19 +0100

    Message par default

--- 2BeSH/Common/include/common.h
+++ 2BeSH/Common/include/common.h
@@ -3,19 +3,22 @@
 
 # include <unistd.h>
 
-enum	e_bool { true = 1, false = 0 };
 typedef enum e_bool	t_bool;
+
+enum	e_bool { true = 1, false = 0 };
 
 t_bool	ft_isprint(unsigned char c);
 t_bool	ft_isinarray(char c, char *ref);
+
+size_t	ft_strlen(char *str);
+
 int		ft_findfirstof(char *str, char *ref);
 int		ft_tabstrlen(char **tab);
+int		ft_strncmp(char *str, char *str2, size_t n);
+int		ft_strcmp(char *str, char *str2);
 
 void	ft_bzero(void *ptr, int size);
 void	*ft_memalloc(int size);
-int		ft_strncmp(char *str, char *str2, size_t n);
-int		ft_strcmp(char *str, char *str2);
-size_t	ft_strlen(char *str);
 
 #endif
 
