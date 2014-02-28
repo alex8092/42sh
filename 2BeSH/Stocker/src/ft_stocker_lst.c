f9c42c3b842b9719050021599b9c86020a92d66f
Theotime RIVIERE <triviere@e1r7p4.42.fr>
2014-02-28 18:51:33 +0100

    Message par default

--- 2BeSH/Stocker/src/ft_stocker_lst.c
+++ 2BeSH/Stocker/src/ft_stocker_lst.c
@@ -15,24 +15,3 @@ t_lst_stocker	*p_stocker_new_lst_el(char c)
 	stocker->m_length++;
 	return (el);
 }
-
-char			*p_stocker_to_string(void)
-{
-	t_stocker		*stocker;
-	t_lst_stocker	*cursor;
-	char			*str;
-	int				i;
-
-	i = 0;
-	stocker = stocker_singleton();
-	str = (char*)malloc(sizeof(sizeof(char) * (stocker->m_length + 1)));
-	cursor = stocker->m_start;
-	while (cursor)
-	{
-		str[i] = cursor->c;
-		i++;
-		cursor = cursor->next;
-	}
-	str[i] = 0;
-	return (str);
-}
