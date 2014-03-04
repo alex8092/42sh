626be9da2a1a8e46dc607316accdc6271101dad9
Thibault RIVIERE <thrivier@e3r1p8.42.fr>
2014-03-04 23:19:14 +0100

    Message par default

--- 2BeSH/Lexer/src/ft_lexer_lex.c
+++ 2BeSH/Lexer/src/ft_lexer_lex.c
@@ -45,7 +45,7 @@ t_lex			*ft_lexer_get_lex(t_lex *parent, char **str)
 	debug(1, "\n\t[COMPARE]");
 	while (cur)
 	{
-		printf("compare match : { \"%s\" } <=> pattern { \"%s\" }\n", *str, cur->pattern);
+/*		printf("compare match : { \"%s\" } <=> pattern { \"%s\" }\n", *str, cur->pattern);*/
 		/*debug(5, "compare match : { \\\"", *str, "\\\" } <=> pattern { \\\"", cur->pattern, "\\\" }\n");*/
 		if ((res = ft_regmatch(*str, cur->pattern, &len)) == *str)
 		{
@@ -53,7 +53,7 @@ t_lex			*ft_lexer_get_lex(t_lex *parent, char **str)
 				ft_manage_word(*str, &len);
 			if (len != 0)
 			{
-				printf("\tlen : %ld\n", len);
+/*				printf("\tlen : %ld\n", len);*/
 				/*debug(1, "\t[MATCH]\n");*/
 				*str += len;
 				return (ft_create_lex(parent, ft_strsub((*str) - len, 0, len), cur->op));
@@ -92,7 +92,7 @@ t_lex			*ft_lexer_lex_str(char *str)
 	end = begin;
 	while (end)
 	{
-		printf("part str[%s]\n", end->str);
+/*		printf("part str[%s]\n", end->str);*/
 		end = end->next;
 	}
 	return (begin);
