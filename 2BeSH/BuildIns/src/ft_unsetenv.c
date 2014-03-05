b1925031da886c2cb4dbb5fca2497903023fcfad
Thibault RIVIERE <thrivier@e3r2p16.42.fr>
2014-03-05 09:23:44 +0100

    Message par default

--- 2BeSH/BuildIns/src/ft_unsetenv.c
+++ 2BeSH/BuildIns/src/ft_unsetenv.c
@@ -1,8 +1,26 @@
 #include "ft_buildins_private.h"
+#include "ft_environment.h"
+#include "common.h"
 #include <stdio.h>
 
 void	p_buildins_unsetenv(char **argv)
 {
-	printf("unsetenv exec\n");
-	(void)argv;
+	size_t	size;
+	char	*name;
+	size_t	i;
+
+	i = 1;
+	if (argv[i])
+	{
+		while (argv[i])
+		{
+			size = ft_strlen(argv[i]);
+			name = env_singleton()->get(argv[i]);
+			if (name)
+				env_singleton()->unset(name, size);
+			i++;
+		}
+	}
+	else
+		write(2, "unsetenv: Too few arguments\n", 28);
 }
