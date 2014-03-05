dbebe5d9edf6f9cf5468ad9a81f0747ac13897c3
Theotime RIVIERE <triviere@e3r3p14.42.fr>
2014-03-05 06:30:05 +0100

    Message par default

--- 2BeSH/Event/src/ft_event_map_rm.c
+++ 2BeSH/Event/src/ft_event_map_rm.c
@@ -6,8 +6,8 @@ t_event		*p_event_map_rm_cur(void)
 {
 	if (!stocker_singleton()->is_end())
 	{
-		stocker_singleton()->rm_cur();
 		display_singleton()->rm_cur();
+		stocker_singleton()->rm_cur();
 	}
 	return (event_singleton());
 }
@@ -16,8 +16,8 @@ t_event		*p_event_map_rm_prev(void)
 {
 	if (!stocker_singleton()->is_first())
 	{
-		stocker_singleton()->rm_prev();
 		display_singleton()->rm_prev();
+		stocker_singleton()->rm_prev();
 	}
 	return (event_singleton());
 }
